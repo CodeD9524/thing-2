8a96d5ed112eef5a90749d403ae07ec9c8653dea
Derek <amir.stowe95@gmail.com>
2025-02-09 17:51:28 -0500

    2-2

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1")
+fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=6")
   .then(response => {
     if (!response.ok) {
       throw new Error('Request failed');
     }
-    return response.json(); // Parse the response as JSON
+    return response.json();
   })
   .then(data => {
-    console.log(data); // Do something with the data
+    console.log(data);
+    console.log(data[3].url)
   })
   .catch(error => {
     console.error('An error occurred:', error);
   });
+ 
